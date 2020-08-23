be8b37f8784cd330e6b8e39982abc91581bb1c26
Suchithra T <sucithrathirunthaiyan@gmail.com>
2020-08-23 17:31:40 +0530

    Python notebook

---
+++ Week1_Capstone.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### COURSERA- IBM APPLIED DATA SCIENCE CAPSTONE
+# ##### Week1 - Peer graded Assignment Create a new repository in github and add simple python notebook into it. 
+# 
+
+# In[5]:
+
+
+#import Panda and Numpy libraries
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
